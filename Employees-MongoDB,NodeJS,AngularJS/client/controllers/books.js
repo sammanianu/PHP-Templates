6e6cf702c54aec77d4b9a7ef1398cfa7b7167637
sammanianu <sammanianu123@gmail.com>
2018-05-30 12:12:29 +0530

    Create Add Update Delete Read functions

--- Employees-MongoDB,NodeJS,AngularJS/client/controllers/books.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp');
-myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
-	console.log('BooksController loaded...');
-	$scope.getBooks = function(){
-		$http.get('/api/books').success(function(response){
-			$scope.books = response;
-		});
-	}
-}]);
--- Employees-MongoDB,NodeJS,AngularJS/client/controllers/controllers.js
+++ Employees-MongoDB,NodeJS,AngularJS/client/controllers/controllers.js
@@ -11,4 +11,24 @@ myApp.controller('empController', function($scope, $route, $routeParams, $http){
 			$scope.employee = response.data;
 		});
 	};
+
+	$scope.addEmployee = function(){
+		$http.post('/api/librarySystem/', $scope.employee).then(function(response){
+			window.location.href = '/';
+		});
+	};
+
+	$scope.updateEmployee = function(){
+		var id = $routeParams.id;
+		$http.put('/api/librarySystem/'+ id, $scope.employee).then(function(response){
+			window.location.href = '/';
+		});
+	};
+
+	$scope.deleteEmployee = function(id){
+		var id = id;
+		$http.delete('/api/librarySystem/'+ id).then(function(response){
+			$route.reload();
+		});
+	};
 });
